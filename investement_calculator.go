a98ccd5f6d0fbf1b85e362ca213252372ffeb2b3
unbeatable-abayomi <igwubor@outlook.com>
2026-02-06 10:38:14 +0100

    Added inflation Rate

--- investement_calculator.go
+++ investement_calculator.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
+	const inflationRate = 2.5
 	investmentAmmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 	futureValue := investmentAmmount * math.Pow(1 + expectedReturnRate/100, years)
-
+    futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 	fmt.Println(futureValue)
+    fmt.Println(futureRealValue)
+
 }
