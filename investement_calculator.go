967ded74e510031fa077cc9716f6eaaa3cce455a
unbeatable-abayomi <igwubor@outlook.com>
2026-02-06 03:46:22 +0100

    Complete Investment Calaculator

--- investement_calculator.go
+++ investement_calculator.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello World")
-}
- 
+	var investmentAmmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+	var futureValue = float64(investmentAmmount) * math.Pow(1 + expectedReturnRate/100, float64(years))
 
+	fmt.Println(futureValue)
+}
