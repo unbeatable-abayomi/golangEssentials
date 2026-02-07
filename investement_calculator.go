9d4639a171c334896a2af7d0647848a42f101b8c
unbeatable-abayomi <igwubor@outlook.com>
2026-02-07 14:37:11 +0100

    Simplified Invesment Calculator

--- investement_calculator.go
+++ investement_calculator.go
@@ -8,7 +8,9 @@ import (
 func main() {
 	fmt.Println("Hello World")
 	const inflationRate = 2.5
-	investmentAmmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	years, expectedReturnRate := 10.0, 5.5
+	var investmentAmmount float64                                                                                                                                            
+	fmt.Scan(&investmentAmmount)
 	futureValue := investmentAmmount * math.Pow(1 + expectedReturnRate/100, years)
     futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 	fmt.Println(futureValue)
