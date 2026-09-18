983f4847c28d8b45c3f492e5d55f3fd681d958c8
unbeatable-abayomi <igwubor@outlook.com>
2026-09-18 07:45:28 +0100

    Modified Control Flow To Use Continue

--- bankapp/app.go
+++ bankapp/app.go
@@ -7,6 +7,9 @@ import (
 
 
 func main(){
+
+	var initialBalance float64 = 50000
+ for {
 	fmt.Println("Welcome to Bank")
 	fmt.Println("What do you want to do")
 	fmt.Println("1. Check Balance")
@@ -15,7 +18,7 @@ func main(){
 	fmt.Println("4. Exit")
     fmt.Print("Your Chioce: ")
     var userAction  string
-	var initialBalance float64 = 50000
+	
     fmt.Scan(&userAction)
 
     if userAction == "1"{
@@ -26,7 +29,8 @@ func main(){
 	   fmt.Scan(&despoitAmount)
 	   if despoitAmount <= 0 {
 		fmt.Println("Hi You entered an invalid amount to deposite")
-		return
+		//return
+		continue
 	   }
 	   initialBalance += despoitAmount
 	   fmt.Printf("Here is the current balance: %v\n", initialBalance)
@@ -37,17 +41,23 @@ func main(){
 	   fmt.Scan(&withdramAmount)
 	   	if withdramAmount <= 0 {
 		fmt.Println("Hi You entered an invalid amount to withdraw below 0 or 0")
-		return
+		//return
+		continue
+
 	   }
 
 	   if withdramAmount > initialBalance {
 	fmt.Println("Hi You entered an invalid amount to withdraw above balance")
-		return
+		//return
+		continue
 	   }
 	   initialBalance -= withdramAmount
 	   fmt.Printf("Here is the current balance: %v\n", initialBalance)
 	}else{
 		println("Goodbye....")
+		//return
+		break
 	}
-
+	}
+  fmt.Println("Thank you for Using our Bank")
 }
